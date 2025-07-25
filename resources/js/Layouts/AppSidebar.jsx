b64b3b0bf8a99ce8d38cbbd1e339e72bf3ad74a5
hamid <hamidkarim2002@gmail.com>
2025-07-25 16:06:19 +0800

    logo sidebar changes

--- resources/js/Layouts/AppSidebar.jsx
+++ resources/js/Layouts/AppSidebar.jsx
@@ -251,7 +251,7 @@ const AppSidebar = () => {
 
   return (
     <aside
-      className={`fixed mt-16 flex flex-col lg:mt-0 top-0 px-5 left-0 bg-white dark:bg-gray-900 dark:border-gray-800 text-gray-900 h-screen transition-all duration-300 ease-in-out z-50 border-r border-gray-200 
+      className={`fixed mt-16 flex flex-col lg:mt-0 top-0 px-5 left-0 bg-white dark:bg-gray-900 dark:border-gray-800 text-gray-900 h-screen transition-all duration-300 ease-in-out z-50 border-r border-gray-200
         ${
           isExpanded || isMobileOpen
             ? "w-[290px]"
@@ -274,14 +274,14 @@ const AppSidebar = () => {
             <>
               <img
                 className="dark:hidden"
-                src="/images/logo/logo-ikahwin-light.svg"
+                src={`${import.meta.env.BASE_URL}images/logo/logo-ikahwin-light.svg`}
                 alt="Logo"
                 width={120}
                 height={90}
               />
               <img
                 className="hidden dark:block"
-                src="/images/logo/logo-ikahwin-dark.png"
+                src={`${import.meta.env.BASE_URL}images/logo/logo-ikahwin-dark.png`}
                 alt="Logo"
                 width={120}
                 height={90}
@@ -289,7 +289,7 @@ const AppSidebar = () => {
             </>
           ) : (
             <img
-              src="/images/logo/logo-ikahwin-light.svg"
+              src={`${import.meta.env.BASE_URL}images/logo/logo-ikahwin-light.svg`}
               alt="Logo"
               width={32}
               height={32}
